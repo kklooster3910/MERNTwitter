cebb011eff48c10e2ad43fe9f265e316a33047ba
Ken Klooster <kklooster3910@gmail.com>
2019-08-11 21:03:37 -0400

    commit user auth and validations done

--- app.js
+++ app.js
@@ -2,12 +2,25 @@ const express = require("express");
 const mongoose = require('mongoose');
 const app = express();
 const db = require('./config/keys').mongoURI;
-
-mongoose.connect(db, { useNewUrlParser: true }).then(() => console.log("Connected to MongoDB successfully")).catch(err => console.log(err));
+const users = require('./routes/api/users');
+const tweets = require('./routes/api/tweets');
+const bodyParser = require('body-parser');
+const User = require('./models/User');
 
 const port = process.env.PORT || 5000;
-
-app.get("/", (req, res) => res.send("something something darkside..."));
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+mongoose.connect(db, { useNewUrlParser: true })
+    .then(() => console.log("Connected to MongoDB successfully"))
+        .catch(err => console.log(err));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
+
+app.get("/", (req, res) => {
+    res.send("something something darkside...")
+});
 // app.listen(port, () => console.log(`Server is running on port ${port}`));
 
